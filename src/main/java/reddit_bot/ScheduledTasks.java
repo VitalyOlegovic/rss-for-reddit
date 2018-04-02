65477cb83edacab6c939301dde21522e45d433c6
vitalijzad <vitalij_zad@libero.it>
2018-04-02 20:10:46 +0200

    Cleaned stuff

--- src/main/java/reddit_bot/ScheduledTasks.java
+++ src/main/java/reddit_bot/ScheduledTasks.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import reddit_bot.facebook.FacebookSubmitterService;
 import reddit_bot.reddit.RedditSubmitterService;
 import reddit_bot.service.LinkService;
 
@@ -18,9 +17,6 @@ public class ScheduledTasks {
     @Autowired
     RedditSubmitterService redditSubmitterService;
 
-    @Autowired
-    FacebookSubmitterService facebookSubmitterService;
-
     private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
 
     @Scheduled(cron = "0 45 6 1/1 * ?")
@@ -31,7 +27,6 @@ public class ScheduledTasks {
     @Scheduled(cron = "0 0 7 1/1 * ?")
     public void send(){
         redditSubmitterService.send();
-        facebookSubmitterService.submit();
     }
 
 }
--- src/main/java/reddit_bot/controller/TestController.java
+++ src/main/java/reddit_bot/controller/TestController.java
@@ -7,12 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import reddit_bot.facebook.FacebookSubmitterService;
 
 @Controller
 public class TestController implements ErrorController {
 
-    private final static Logger logger = LoggerFactory.getLogger(FacebookSubmitterService.class);
+    private final static Logger logger = LoggerFactory.getLogger(TestController.class);
 
     private static final String PATH = "/error";
 
