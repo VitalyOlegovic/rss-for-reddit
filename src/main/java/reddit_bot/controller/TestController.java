ee1a3b1d2780af2535b7c0bb9066882f03b6d93a
Vitalij Zadneprovskij <vitalij_zad@libero.it>
2021-03-28 16:32:26 +0200

    Docker settings

--- src/main/java/reddit_bot/controller/TestController.java
+++ src/main/java/reddit_bot/controller/TestController.java
@@ -2,11 +2,14 @@ package reddit_bot.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import reddit_bot.reddit.RedditSubmitterService;
+import reddit_bot.service.LinkService;
 
 @Controller
 public class TestController implements ErrorController {
@@ -14,6 +17,12 @@ public class TestController implements ErrorController {
     private final static Logger logger = LoggerFactory.getLogger(TestController.class);
 
     private static final String PATH = "/error";
+
+    @Autowired
+    LinkService linkService;
+
+    @Autowired
+    RedditSubmitterService redditSubmitterService;
 
     @RequestMapping("/")
     @ResponseBody
@@ -38,4 +47,17 @@ public class TestController implements ErrorController {
     public String getErrorPath() {
         return PATH;
     }
+
+    @RequestMapping("/updateFeeds")
+    @ResponseBody
+    public void updateFeeds(){
+        linkService.updateFeeds();
+    }
+
+    @RequestMapping("/send")
+    @ResponseBody
+    public void send(){
+        redditSubmitterService.send();
+    }
+
 }
